579ac02075359510141d0154537779882ac0956b
usupsuparma <usup@solusiteknologi.co.id>
2021-12-02 21:02:27 +0700

    fixing id

--- src/Listener.js
+++ src/Listener.js
@@ -10,7 +10,7 @@ class Listener {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
 
-      const playlist = await this._playlistsService.getPlaylists("playlist-KM8Hbf072BAPCzi2");
+      const playlist = await this._playlistsService.getPlaylists(playlistId);
       const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
       console.log(result);
     } catch (error) {
