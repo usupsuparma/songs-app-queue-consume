3718e89a9cfa717fc6ffdd307c3164a0621d6758
usupsuparma <usup@solusiteknologi.co.id>
2021-12-02 20:59:55 +0700

    first commit

---
+++ src/PlaylistsService.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+const { mapDBToModelPlaylist, mapDBToModelSong } = require('./utils');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylists(playlistId) {
+    console.log(playlistId);
+    const query = {
+      text: `SELECT playlists.*, songs.* FROM playlists
+      LEFT JOIN playlistsongs ON playlistsongs.playlist_id = playlists.id
+      LEFT JOIN songs ON songs.id = playlistsongs.song_id
+      WHERE playlists.id = $1 
+      GROUP BY playlists.id, songs.id`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows.map(mapDBToModelSong);
+  }
+}
+
+module.exports = PlaylistsService;
---
+++ src/utils.js
@@ -0,0 +1,18 @@
+const mapDBToModelPlaylist = ({
+  id, name, username,
+}) => ({
+  id,
+  name,
+  username,
+});
+
+const mapDBToModelSong = ({ id, title, performer }) => ({
+  id,
+  title,
+  performer,
+});
+
+module.exports = {
+  mapDBToModelPlaylist,
+  mapDBToModelSong
+}
